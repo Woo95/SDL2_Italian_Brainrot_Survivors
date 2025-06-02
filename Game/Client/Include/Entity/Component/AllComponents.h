25250d0b742c24f9016839e45e9b0cf61a0567fb
Woo95 <chaewan2002@gmail.com>
2025-06-03 05:00:32 +0900

    Added AllComponents.h

---
+++ Game/Client/Include/Entity/Component/AllComponents.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Collider/BoxCollider.h"
+#include "Collider/CircleCollider.h"
+#include "MovementComponent.h"
+#include "SpriteComponent.h"
+#include "WidgetComponent.h"
+#include "Rigidbody.h"
+
+#include "ScrollMapComponent.h"
